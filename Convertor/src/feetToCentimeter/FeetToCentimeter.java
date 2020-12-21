228510cfe052cd9bc22acc3c518ee73c45e9fcf9
vamshigoud077 <50431439+vamshigoud077@users.noreply.github.com>
2020-12-21 17:40:09 -0500

    Add files via upload

---
+++ Convertor/src/feetToCentimeter/FeetToCentimeter.java
@@ -0,0 +1,17 @@
+package feetToCentimeter;
+
+public class FeetToCentimeter {
+	
+	
+	public double unitsConvertor(double d) {
+		System.out.println("double is called here:");
+		return d * 30.48;
+		
+	}
+
+	public double  unitsConvertor(int i) {
+		System.out.println("int value is called here:");
+		return  (i * 30.48);
+		
+	}
+}
---
+++ Convertor/src/feetToCentimeter/Main.java
@@ -0,0 +1,12 @@
+package feetToCentimeter;
+
+public class Main {
+	public static void main(String args[]) {
+		
+	
+	FeetToCentimeter fc= new FeetToCentimeter();
+	System.out.println("after converting feet into centimeter the value is:"+fc.unitsConvertor(18));
+	System.out.println("after converting feet into centimeter the value is:"+fc.unitsConvertor(300.98));
+
+}
+}
